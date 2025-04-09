e8706eb35ed82b9466cd0d7e19179d00902d2916
aiuyap <186591613+aiuyap@users.noreply.github.com>
2025-04-10 02:41:21 +0800

    feat: separate navbar into its own component

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -1,5 +1,4 @@
 import { Button } from "./ui/button";
-import heroImg from "../assets/hero.jpg";
 
 export function Navbar() {
   return (
@@ -27,19 +26,6 @@ export function Navbar() {
           </li>
         </ul>
       </nav>
-      <section
-        className="bg-cover bg-center"
-        style={{
-          backgroundImage: `url('${heroImg}')`,
-          height: "calc(100vh - 68px)",
-        }}
-      >
-        <div className="flex h-full w-full items-center justify-center bg-black">
-          <h1 className="text-center text-4xl font-bold text-white md:text-6xl">
-            Book your next staycation with us.
-          </h1>
-        </div>
-      </section>
     </>
   );
 }
