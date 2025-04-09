dbc6c5226c4f3c44ecdef169e02b9b0875ea5561
aiuyap <186591613+aiuyap@users.noreply.github.com>
2025-04-10 03:17:17 +0800

    refactor: changed navbar texts color

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -3,21 +3,21 @@ import { Button } from "./ui/button";
 export function Navbar() {
   return (
     <>
-      <nav className="flex items-center justify-between bg-white p-4">
-        <div className="text-xl font-bold text-gray-800">Condo Booking</div>
-        <ul className="flex items-center space-x-6 font-medium text-gray-600">
+      <nav className="flex items-center justify-between bg-black p-4">
+        <div className="text-xl font-bold text-white">Condo Booking</div>
+        <ul className="flex items-center space-x-6 font-medium text-white">
           <li>
-            <a href="#" className="transition-colors hover:text-blue-600">
+            <a href="#" className="transition-colors hover:text-red-500">
               Home
             </a>
           </li>
           <li>
-            <a href="#" className="transition-colors hover:text-blue-600">
+            <a href="#" className="transition-colors hover:text-red-500">
               Condos
             </a>
           </li>
           <li>
-            <a href="#" className="transition-colors hover:text-blue-600">
+            <a href="#" className="transition-colors hover:text-red-500">
               About
             </a>
           </li>
