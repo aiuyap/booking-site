91801169118dd664a9f1dd17074a2c4e77be53b0
aiuyap <186591613+aiuyap@users.noreply.github.com>
2025-04-10 03:11:02 +0800

    feat: managed to add hero bg and changed ui theme

--- src/components/hero.component.tsx
+++ src/components/hero.component.tsx
@@ -1,15 +1,20 @@
+import heroImg from "../assets/hero.jpg";
+import { Button } from "./ui/button";
+
 function Hero() {
   return (
     <section
       className="bg-cover bg-center"
       style={{
+        backgroundImage: `url(${heroImg})`,
         height: "calc(100vh - 68px)",
       }}
     >
-      <div className="flex h-full w-full items-center justify-center bg-black">
-        <h1 className="text-center text-4xl font-bold text-white md:text-6xl">
+      <div className="flex h-full w-full flex-col items-center justify-center gap-5 bg-black/70">
+        <h1 className="text-center text-4xl font-bold text-white opacity-100 md:text-6xl">
           Book your next staycation with us.
         </h1>
+        <Button>Book Now</Button>
       </div>
     </section>
   );
